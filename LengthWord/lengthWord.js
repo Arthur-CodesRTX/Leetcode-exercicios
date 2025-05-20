3fc7b55612e4cede9055b01b0969ef09b356c0d9
Arthur-CodesRTX <poeta.codes24@gmail.com>
2025-05-20 00:45:20 -0300

    lengthWord

---
+++ LengthWord/lengthWord.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLastWord = function(s) {
+    
+    let fraseLimpa = s.trim().replace(/\s+/g, ' ');
+    let fraseArr = fraseLimpa.split(" ")
+    return fraseArr[fraseArr.length - 1].length
+};
+
+
+console.log(lengthOfLastWord("Hello World"))
+console.log(lengthOfLastWord("   fly me   to   the moon  "))
+console.log(lengthOfLastWord("luffy is still joyboy"))
