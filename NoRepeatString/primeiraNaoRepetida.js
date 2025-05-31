3e1919ed1a4ed5bc9e8355da747e1c65ee3facc0
Arthur-CodesRTX <poeta.codes24@gmail.com>
2025-05-31 01:25:19 -0300

    priemrianNaoRepetida

---
+++ NoRepeatString/primeiraNaoRepetida.js
@@ -0,0 +1,24 @@
+function primeiraNaoRepetida(s){
+
+    let repetidos = new Set()
+
+    for(let i = 0; i < s.length; i++){
+        for(let j = 0; j < s.length; j++){
+            if(i !== j && s[i] === s[j]){
+                repetidos.add(s[i])
+                break;
+            }
+        }
+
+        if(!repetidos.has(s[i])){
+            return s[i]
+        }
+    }
+
+    return null
+   
+}
+
+console.log(primeiraNaoRepetida("abacabad"))
+console.log(primeiraNaoRepetida("abacabaabacaba"))
+console.log(primeiraNaoRepetida("aabccdee"))
